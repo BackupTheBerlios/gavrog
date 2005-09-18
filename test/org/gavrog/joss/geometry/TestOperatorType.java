c96b7bfb3829efc41cb1326d3835a85d4ddf8b34
odf <devnull@localhost>
2005-09-18 03:22:34 +0000

    Added more test cases.

--- test/org/gavrog/joss/geometry/TestOperatorType.java
+++ test/org/gavrog/joss/geometry/TestOperatorType.java
@@ -18,45 +18,61 @@ package org.gavrog.joss.geometry;
 
 import junit.framework.TestCase;
 
-import org.gavrog.jane.compounds.Matrix;
-
 /**
  * @author Olaf Delgado
- * @version $Id: TestOperatorType.java,v 1.1 2005/09/18 02:33:46 odf Exp $
+ * @version $Id: TestOperatorType.java,v 1.2 2005/09/18 03:22:34 odf Exp $
  */
 public class TestOperatorType extends TestCase {
     final OperatorType ot1 = new OperatorType(new Operator("-y,x"));
-    final Matrix M1 = new Matrix(new int[][] { { 0, 1 }, { -1, 0 } });
     final OperatorType ot2 = new OperatorType(new Operator("y,x"));
-    final Matrix M2 = new Matrix(new int[][] { { 0, 1 }, { 1, 0 } });
-
-    public void testGetAxisMatrix() {
-        assertNull(OperatorType.getAxis(M1));
-        assertTrue(OperatorType.getAxis(M2).isCollinearTo(new Vector(new int[] {1, 1})));
-    }
-
-    public void testMatrixOrder() {
-       assertEquals(4, OperatorType.matrixOrder(M1, 6));
-       assertEquals(2, OperatorType.matrixOrder(M2, 6));
-    }
+    final OperatorType ot3 = new OperatorType(new Operator("z,x,y"));
+    final OperatorType ot4 = new OperatorType(new Operator("y,z,x"));
+    final OperatorType ot5 = new OperatorType(new Operator("x,y"));
+    final OperatorType ot6 = new OperatorType(new Operator("x,y,z"));
+    final OperatorType ot7 = new OperatorType(new Operator("y,-x"));
+    final OperatorType ot8 = new OperatorType(new Operator("y,z,x"));
 
     public void testGetAxis() {
         assertNull(ot1.getAxis());
         assertTrue(ot2.getAxis().isCollinearTo(new Vector(new int[] {1, 1})));
+        assertTrue(ot3.getAxis().isCollinearTo(new Vector(new int[] {1, 1, 1})));
+        assertTrue(ot4.getAxis().isCollinearTo(new Vector(new int[] {1, 1, 1})));
+        assertNull(ot5.getAxis());
+        assertNull(ot6.getAxis());
+        assertNull(ot7.getAxis());
+        assertTrue(ot8.getAxis().isCollinearTo(new Vector(new int[] {1, 1, 1})));
     }
 
     public void testIsClockwise() {
         assertTrue(ot1.isClockwise());
         // isClockwise() is irrelevant for ot2
+        assertTrue(ot3.isClockwise());
+        assertFalse(ot4.isClockwise());
+        assertTrue(ot5.isClockwise());
+        assertTrue(ot6.isClockwise());
+        assertFalse(ot7.isClockwise());
+        assertFalse(ot8.isClockwise());
     }
 
     public void testGetOrder() {
         assertEquals(4, ot1.getOrder());
         assertEquals(2, ot2.getOrder());
+        assertEquals(3, ot3.getOrder());
+        assertEquals(3, ot4.getOrder());
+        assertEquals(1, ot5.getOrder());
+        assertEquals(1, ot6.getOrder());
+        assertEquals(4, ot7.getOrder());
+        assertEquals(3, ot8.getOrder());
     }
 
     public void testIsOrientationPreserving() {
         assertTrue(ot1.isOrientationPreserving());
         assertFalse(ot2.isOrientationPreserving());
+        assertTrue(ot3.isOrientationPreserving());
+        assertTrue(ot4.isOrientationPreserving());
+        assertTrue(ot5.isOrientationPreserving());
+        assertTrue(ot6.isOrientationPreserving());
+        assertTrue(ot7.isOrientationPreserving());
+        assertTrue(ot8.isOrientationPreserving());
     }
 }
