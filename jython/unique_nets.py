69b5b7811062c80cd875a54ca32cee35ceed64e5
odf <devnull@localhost>
2005-11-18 00:45:04 +0000

    Added many things.

--- jython/unique_nets.py
+++ jython/unique_nets.py
@@ -1,29 +1,113 @@
+#!/bin/env jython
+
+# ============================================================
+#   Imports
+# ============================================================
+
+# --- Jython stuff
 import sys
 
-from java.io import FileReader
+# --- Java stuff
+from java.lang import ClassLoader
+from java.io import InputStreamReader, BufferedReader
 
+# --- Gavrog stuff
 from org.gavrog.joss.pgraphs.io import NetParser
+from org.gavrog.systre import Archive
 
+
+# ============================================================
+#   Prepare for RCSR lookup
+# ============================================================
+
+# --- get RCSR archive file (possibly from a .jar or the web)
+rcsr_path = "org/gavrog/systre/rcsr.arc"
+rcsr_stream = ClassLoader.getSystemResourceAsStream(rcsr_path)
+reader = BufferedReader(InputStreamReader(rcsr_stream))
+
+# --- create an archive object from it
+archive = Archive("1.0")
+archive.addAll(reader)
+
+
+# ============================================================
+#   Main data processing
+# ============================================================
+
+# --- create a parser that reads nets from the given file
+parser = NetParser(sys.argv[1])
+
+# --- dictionary of seen nets
 seen = {}
-parser = NetParser(FileReader(sys.argv[1]))
+
+# --- count the nets that we read
 count = 0
 
+# --- main loop
 while 1:
-    G = parser.parseNet()
-    if G is None:
+    # --- read the next net from the file
+    G0 = parser.parseNet()
+    if G0 is None:
         break
     count += 1
 
+    # --- retrieve the net's name or make one up
     name = parser.name
     if name is None:
         name = "unamed-%03d" % count
     print "Net %03d (%s): " % (count, name),
-    
+
+    # --- IMPORTANT: use the minimal ideal translation unit
+    G = G0.minimalImage()
+
+    # --- compute the Systre key and check if we've seen it
     key = G.systreKey
     old = seen.get(key)
     if old:
-        print "\t\tduplicates %s" % old
+        # --- skip further output for duplicates
+        print "\tduplicates %s" % old
+        print
+        continue
     else:
-        print "\t\tnew entry"
-
+        print "\tnew entry"
     seen[key] = name
+
+    # --- print coordination sequences for the unique nodes
+    print "\tCS:",
+    n = 0
+    for orb in G.nodeOrbits():
+        for v in orb:
+            i = 0
+            for x in G.coordinationSequence(v):
+                if i == 0:
+                    if n == 0:
+                        print "\t%d" % x,
+                    else:
+                        print "\t\t%d" % x,
+                else:
+                    print x,
+                i += 1
+                if i > 16:
+                    break
+            break
+        print
+        n += 1
+
+    # --- print the name of the space group
+    print "\tGroup:",
+    print "\t%s" % G.spaceGroup.name
+
+    # --- look net up in RCSR and print symbol, if found
+    print "\tRCSR:",
+    found = archive.get(key)
+    if found:
+        print "\t%s" % found.name
+    else:
+        print "\tnot found"
+
+    # --- don't crowd
+    print
+
+# ============================================================
+#   EOF
+# ============================================================
