13ac2ccd1b6a0fbed3c61d97a2effab7d08f7d30
odf <devnull@localhost>
2006-01-30 23:35:41 +0000

    Added comments and a verification for the spacegroup recognition code.

--- src/org/gavrog/systre/Demo.java
+++ src/org/gavrog/systre/Demo.java
@@ -50,7 +50,7 @@ import org.gavrog.joss.pgraphs.io.NetParser;
  * First preview of the upcoming Gavrog version of Systre.
  * 
  * @author Olaf Delgado
- * @version $Id: Demo.java,v 1.20 2006/01/27 05:05:36 odf Exp $
+ * @version $Id: Demo.java,v 1.21 2006/01/30 23:35:41 odf Exp $
  */
 public class Demo {
     private final static DecimalFormat fmtReal4 = new DecimalFormat("0.0000");
@@ -73,9 +73,16 @@ public class Demo {
     }
     
     
+    /**
+     * Analyzes all nets specified in a file and prints the results.
+     * 
+     * @param filename the name of the input file.
+     */
     public static void run(final String filename) {
+        // --- set the output stream
         final PrintStream out = System.out;
         
+        // --- read the default archive
         final Package pkg = Archive.class.getPackage();
         final String packagePath = pkg.getName().replaceAll("\\.", "/");
         final String archivePath = packagePath + "/rcsr.arc";
@@ -84,6 +91,7 @@ public class Demo {
         final BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
         rcsr.addAll(reader);
         
+        // --- set up a parser for reading input from the given file
         NetParser parser = null;
         int count = 0;
         try {
@@ -92,15 +100,18 @@ public class Demo {
             System.err.println("Could not find file \"" + filename + "\"");
             return;
         }
-        
         out.println("Data file \"" + filename + "\".");
         
+        // --- loop through the structures specied in the input file
         while (true) {
+            // --- read the next net
             PeriodicGraph G = parser.parseNet();
             if (G == null) {
                 break;
             }
             ++count;
+            
+            // --- print some information on net as given
             final String name = parser.getName();
             if (name == null) {
                 out.println("Nameless structure.");
@@ -110,7 +121,6 @@ public class Demo {
             out.println();
             out.println("   Given space group is " + parser.getSpaceGroup() + ".");
             out.flush();
-
             final int n = G.numberOfNodes();
             final int m = G.numberOfEdges();
             out.println("   " + n + " vert" + (n > 1 ? "ices" : "ex") + " and "
@@ -118,6 +128,7 @@ public class Demo {
             out.println();
             out.flush();
 
+            // --- test if it is Systre-compatible
             if (!G.isConnected()) {
                 out.println("   Sorry, graph is not connected! Giving up.");
                 out.println();
@@ -130,7 +141,8 @@ public class Demo {
                 out.flush();
                 continue;
             }
-            
+
+            // --- determine a minimal repeat unit
             G = G.minimalImage();
             final int r = n / G.numberOfNodes();
             if (r > 1) {
@@ -140,7 +152,8 @@ public class Demo {
                 out.println("   Given primitive cell is accurate.");
             }                
             out.flush();
-            
+
+            // --- determine the ideal symmetries
             final List ops = G.symmetryOperators();
             out.println("   point group has " + ops.size() + " elements.");
             out.flush();
@@ -148,6 +161,7 @@ public class Demo {
             out.println("   " + k + " kind" + (k > 1 ? "s" : "") + " of vertex.");
             out.flush();
             
+            // --- determine the coordination sequences
             out.println("   Coordination sequences:");
             for (final Iterator orbits = G.nodeOrbits(); orbits.hasNext();) {
                 final Set orbit = (Set) orbits.next();
@@ -163,10 +177,13 @@ public class Demo {
             out.println();
             out.flush();
 
+            // --- find the space group name and conventional settings
             final int d = G.getDimension();
             final SpaceGroup group = new SpaceGroup(d, ops);
             final SpaceGroupFinder finder = new SpaceGroupFinder(group);
             final String groupName = finder.getGroupName();
+            final CoordinateChange toStd = finder.getToStd();
+            final CoordinateChange fromStd = (CoordinateChange) toStd.inverse();
             out.println("   Ideal space group is " + groupName + ".");
             final String givenName = SpaceGroupCatalogue.normalizedName(parser
                     .getSpaceGroup());
@@ -177,6 +194,20 @@ public class Demo {
             out.println();
             out.flush();
 
+            // --- verify the output of the spacegroup finder
+            final CoordinateChange trans = SpaceGroupCatalogue.transform(d, groupName);
+            if (!trans.isOne()) {
+                final String msg = "Internal error: converted to non-conventional setting";
+                throw new RuntimeException(msg);
+            }
+            final Set conventionalOps = new SpaceGroup(d, groupName).primitiveOperators();
+            final Set probes = new SpaceGroup(d, toStd.applyTo(ops)).primitiveOperators();
+            if (!probes.equals(conventionalOps)) {
+                final String msg = "Internal error: spacegroup finder messed up operators";
+                throw new RuntimeException(msg);
+            }
+            
+            // --- determine the Systre key and look it up in the archive
             final String invariant = G.invariant().toString();
             final Archive.Entry found = rcsr.getByKey(invariant);
             if (found == null) {
@@ -186,13 +217,16 @@ public class Demo {
                 out.println("   Name: " + found.getName());
             }
             out.flush();
-            
+
+            // --- bail out - for now - if not a 3d structure
             if (d != 3) {
                 out.println("Sorry, currently no refined output for dimension != 3.");
                 out.println();
                 out.flush();
                 continue;
             }
+
+            // --- relax the structure from the barycentric embedding (EXPERIMENTAL CODE)
             SpringEmbedder embedder = new SpringEmbedder(G);
             try {
                 embedder.steps(200);
@@ -205,8 +239,8 @@ public class Demo {
                 embedder.steps(200);
             }
             embedder.normalize();
-            final CoordinateChange toStd = finder.getToStd();
-            final CoordinateChange fromStd = (CoordinateChange) toStd.inverse();
+
+            // --- print the results of the relaxation in the conventional setting
             final Matrix gram = embedder.getGramMatrix();
             final Vector x = (Vector) Vector.unit(3, 0).times(fromStd);
             final Vector y = (Vector) Vector.unit(3, 1).times(fromStd);
@@ -223,6 +257,7 @@ public class Demo {
             final Real gamma = (Real) ((Real) Vector.dot(x, y, gram)
                     .dividedBy(a.times(b))).acos().times(f);
 
+            //    ... print the cell parameters
             out.println("   Refined cell parameters:");
             out.println("       a = " + fmtReal5.format(a.doubleValue()) + ", b = "
                         + fmtReal5.format(b.doubleValue()) + ", c = "
@@ -231,6 +266,8 @@ public class Demo {
                         + ", beta = " + fmtReal4.format(beta.doubleValue())
                         + ", gamma = " + fmtReal4.format(gamma.doubleValue()));
             final Map pos = embedder.getPositions();
+            
+            //    ... print the atom positions
             out.println("   Refined atom positions:");
             for (final Iterator orbits = G.nodeOrbits(); orbits.hasNext();) {
                 final Set orbit = (Set) orbits.next();
@@ -242,6 +279,8 @@ public class Demo {
                 }
                 out.println();
             }
+            
+            //    ... print the edges
             out.println("   Edges:");
             for (final Iterator orbits = G.edgeOrbits(); orbits.hasNext();) {
                 final Set orbit = (Set) orbits.next();
@@ -263,14 +302,20 @@ public class Demo {
                 }
                 out.println();
             }
-
             out.println();
+
+            // --- some blank lines as separators
             out.println();
             out.println();
             out.flush();
         }
     }
     
+    /**
+     * The main method takes the first command line argument and passes it to {@link #run}.
+     * 
+     * @param args the command line arguments.
+     */
     public static void main(final String args[]) {
         run(args[0]);
     }
