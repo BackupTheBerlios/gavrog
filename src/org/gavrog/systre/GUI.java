a66935cf01bbf77115f36f9a17ceb1c16b8c0960
odf <devnull@localhost>
2006-03-24 22:24:53 +0000

    Data files can now be opened and output can be saved.

--- src/org/gavrog/systre/GUI.java
+++ src/org/gavrog/systre/GUI.java
@@ -18,36 +18,50 @@ package org.gavrog.systre;
 
 import java.awt.Color;
 import java.awt.Insets;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.Writer;
 
 import javax.swing.SwingUtilities;
 
 import buoy.event.CommandEvent;
 import buoy.event.WindowClosingEvent;
 import buoy.widget.BButton;
+import buoy.widget.BFileChooser;
 import buoy.widget.BFrame;
 import buoy.widget.BLabel;
 import buoy.widget.BScrollBar;
 import buoy.widget.BScrollPane;
+import buoy.widget.BStandardDialog;
 import buoy.widget.BTextArea;
 import buoy.widget.BorderContainer;
+import buoy.widget.GridContainer;
 import buoy.widget.LayoutInfo;
 
 /**
  * A simple GUI for Gavrog Systre (in the making).
  * 
  * @author Olaf Delgado
- * @version $Id: GUI.java,v 1.2 2006/03/24 18:36:23 odf Exp $
+ * @version $Id: GUI.java,v 1.3 2006/03/24 22:24:53 odf Exp $
  */
 public class GUI extends BFrame {
 	final private static Color textColor = new Color(255, 250, 240);
 	final private static Color buttonColor = new Color(224, 224, 240);
-	private BTextArea output;
+
+    private final BFileChooser inFileChooser = new BFileChooser(BFileChooser.OPEN_FILE,
+            "Open data file");
+    private final BFileChooser outFileChooser = new BFileChooser(BFileChooser.SAVE_FILE,
+            "Save output");
+    
+    private final Demo systre = new Demo();
+
+    private BTextArea output;
 	private BScrollBar vscroll;
-	private PrintStream stdout;
-	private PrintStream stderr;
+    private BButton openButton;
+    private BButton saveButton;
 
     /**
      * Constructs an instance.
@@ -57,7 +71,7 @@ public class GUI extends BFrame {
 		
 		final BorderContainer main = new BorderContainer();
 		main.setDefaultLayout(new LayoutInfo(LayoutInfo.CENTER, LayoutInfo.BOTH, null,
-				null));
+                null));
 		main.setBackground(textColor);
 
 		final BorderContainer top = new BorderContainer();
@@ -68,9 +82,15 @@ public class GUI extends BFrame {
 				+ "Version 1.0 beta 0<br><br>"
 				+ "by Olaf Delgado-Friedrichs 2001-2006</html>");
 		top.add(label, BorderContainer.NORTH);
-		final BButton openButton = makeButton("Open file");
-		openButton.setEnabled(false);
-		top.add(openButton, BorderContainer.SOUTH, new LayoutInfo(LayoutInfo.CENTER,
+        
+        final GridContainer buttonBar = new GridContainer(2, 1);
+        buttonBar.setDefaultLayout(new LayoutInfo(LayoutInfo.CENTER,
+                LayoutInfo.HORIZONTAL, null, null));
+        
+        buttonBar.add(openButton = makeButton("Open"), 0, 0);
+        buttonBar.add(saveButton = makeButton("Save"), 1, 0);
+        
+        top.add(buttonBar, BorderContainer.SOUTH, new LayoutInfo(LayoutInfo.CENTER,
 				LayoutInfo.HORIZONTAL, null, null));
 		main.add(top, BorderContainer.NORTH);
 		
@@ -90,8 +110,10 @@ public class GUI extends BFrame {
         
         setContent(main);
         
-        captureOutput(true);
+        captureOutput();
         
+        openButton.addEventLink(CommandEvent.class, this, "doOpen");
+        saveButton.addEventLink(CommandEvent.class, this, "doSave");
         okButton.addEventLink(CommandEvent.class, this, "doQuit");
         addEventLink(WindowClosingEvent.class, this, "doQuit");
         
@@ -105,30 +127,87 @@ public class GUI extends BFrame {
     	return button;
     }
     
-    private void captureOutput(final boolean capture) {
-    	if (capture) {
-    		this.stdout = System.out;
-    		this.stderr = System.err;
-    		
-    		final PrintStream stream = new PrintStream(new OutputStream() {
-				public void write(int b) throws IOException {
-					output.append(new String(new char[] { (char) b }));
-					if ((char) b == '\n') {
-						// --- finally found out what invokeLater is for :)
-						SwingUtilities.invokeLater(new Runnable() {
-							public void run() {
-								vscroll.setValue(vscroll.getMaximum());
-							}
-						});
-					}
-				}
-			});
-			System.setOut(stream);
-			System.setErr(stream);
-    	} else { 
-    		System.setOut(stdout);
-    		System.setErr(stderr);
-    	}
+    private void captureOutput() {
+        final OutputStream stream = new OutputStream() {
+            public void write(int b) throws IOException {
+                output.append(new String(new char[] { (char) b }));
+                if ((char) b == '\n') {
+                    // --- finally found out what invokeLater is for :)
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            vscroll.setValue(vscroll.getMaximum());
+                        }
+                    });
+                }
+            }
+        };
+
+        this.systre.setOutStream(new PrintStream(stream));
+    }
+    
+    protected void doOpen() {
+        final boolean success = this.inFileChooser.showDialog(this);
+        if (success) {
+            final String filename = this.inFileChooser.getSelectedFile().getName();
+            final File dir = this.inFileChooser.getDirectory();
+            final String path = new File(dir, filename).getAbsolutePath();
+            this.output.setText("");
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        SwingUtilities.invokeAndWait(new Runnable() {
+                            public void run() {
+                                disableButtons();
+                            }
+                        });
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    systre.processDataFile(path);
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            enableButtons();
+                        }
+                    });
+                }
+            }).start();
+        }
+    }
+    
+    protected void doSave() {
+        final String name = systre.getLastFileNameWithoutExtension();
+        this.outFileChooser.setSelectedFile(new File(name + ".out"));
+        final boolean success = this.outFileChooser.showDialog(this);
+        if (success) {
+            final String filename = this.outFileChooser.getSelectedFile().getName();
+            final File dir = this.outFileChooser.getDirectory();
+            final File file = new File(dir, filename);
+            if (file.exists()) {
+                final int choice = new BStandardDialog("File exists", "File \"" + file
+                        + "\" already exists. Overwrite?", BStandardDialog.WARNING)
+                        .showOptionDialog(this, new String[] { "yes", "no" }, "no");
+                if (choice > 0) {
+                    return;
+                }
+            }
+            try {
+                final Writer writer = new FileWriter(file);
+                writer.write(this.output.getText());
+                writer.flush();
+                writer.close();
+            } catch (IOException ex) {
+            }
+        }
+    }
+    
+    private void disableButtons() {
+        this.openButton.setEnabled(false);
+        this.saveButton.setEnabled(false);
+    }
+    
+    private void enableButtons() {
+        this.openButton.setEnabled(true);
+        this.saveButton.setEnabled(true);
     }
     
     protected void doQuit() {
@@ -137,7 +216,5 @@ public class GUI extends BFrame {
     
     public static void main(final String args[]) {
         new GUI();
-        final Demo demo = new Demo();
-        demo.processDataFile(args[0]);
     }
 }
