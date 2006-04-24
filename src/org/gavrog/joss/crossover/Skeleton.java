1411a1f322f35ada4343c97d7bc7a955d4819bfb
odf <devnull@localhost>
2006-04-24 22:13:21 +0000

    Finished implementation and debugged.

--- src/org/gavrog/joss/crossover/Skeleton.java
+++ src/org/gavrog/joss/crossover/Skeleton.java
@@ -28,6 +28,7 @@ import org.gavrog.jane.compounds.Matrix;
 import org.gavrog.jane.fpgroups.FpGroup;
 import org.gavrog.jane.fpgroups.FreeWord;
 import org.gavrog.joss.dsyms.basic.DelaneySymbol;
+import org.gavrog.joss.dsyms.basic.Traversal;
 import org.gavrog.joss.dsyms.derived.Covers;
 import org.gavrog.joss.dsyms.derived.FundamentalGroup;
 import org.gavrog.joss.geometry.Vector;
@@ -38,9 +39,17 @@ import org.gavrog.joss.pgraphs.basic.PeriodicGraph;
  * Represents a periodic graph derived as the 1-skeleton of a tiling.
  * 
  * @author Olaf Delgado
- * @version $Id: Skeleton.java,v 1.1 2006/04/24 02:50:28 odf Exp $
+ * @version $Id: Skeleton.java,v 1.2 2006/04/24 22:13:21 odf Exp $
  */
 public class Skeleton extends PeriodicGraph {
+    final private Map edgeToWord;
+    final private Vector L[];
+    
+	/**
+	 * Constructs an instance as the skeleton of a given tiling.
+	 * 
+	 * @param ds the Delaney symbol of the source tiling.
+	 */
 	public Skeleton(final DelaneySymbol ds) {
 		super(ds.dim());
 		
@@ -63,6 +72,7 @@ public class Skeleton extends PeriodicGraph {
 		
 		// --- compute the fundamental group and find a translation representation for it
         final FundamentalGroup FG = new FundamentalGroup(cov);
+        this.edgeToWord = FG.getEdgeToWord();
         final FpGroup G = FG.getPresentation();
         final Matrix N = LinearAlgebra.columnNullSpace(G.relatorMatrix(), false);
         if (N.numberOfColumns() != this.dimension) {
@@ -70,12 +80,12 @@ public class Skeleton extends PeriodicGraph {
         	throw new RuntimeException(msg);
         }
         // --- extract the translation vectors
-        final Vector L[] = Vector.fromMatrix(N);
+        this.L = Vector.fromMatrix(N);
         
         // --- set up index lists
         final List nodeIdcs = new LinkedList();
         final List edgeIdcs = new LinkedList();
-        for (int i = 0; i < dim; ++i) {
+        for (int i = 0; i <= dim; ++i) {
         	if (i != 0) {
         		nodeIdcs.add(new Integer(i));
         	}
@@ -85,37 +95,65 @@ public class Skeleton extends PeriodicGraph {
         }
         
         // --- create nodes of the graph and maps between nodes and Delaney chambers
-        final Map v2ch = new HashMap();
         final Map ch2v = new HashMap();
         for (final Iterator iter = cov.orbitRepresentatives(nodeIdcs); iter.hasNext();) {
         	final Object D = iter.next();
         	final INode v = newNode();
-        	v2ch.put(v, D);
         	for (final Iterator orbit = cov.orbit(nodeIdcs, D); orbit.hasNext();) {
         		ch2v.put(orbit.next(), v);
         	}
         }
         
+        // --- map chambers to translations w.r.t. node orbit representatives
+        Traversal trav = new Traversal(cov, nodeIdcs, cov.orbitRepresentatives(nodeIdcs));
+        HashMap trans = new HashMap();
+        while (trav.hasNext()) {
+            final Pair e = (Pair) trav.next();
+            int i = ((Integer) e.getFirst()).intValue();
+            final Object D = e.getSecond();
+            if (i < 0) {
+                trans.put(D, Vector.zero(dim));
+            } else {
+                final Object Di = cov.op(i, D);
+                final Vector v = (Vector) trans.get(Di);
+                trans.put(D, v.plus(edgeTranslation(i, Di)));
+            }
+        }
+        
         // --- create the edges
-        final Map e2w = FG.getEdgeToWord();
         for (final Iterator iter = cov.orbitRepresentatives(edgeIdcs); iter.hasNext();) {
         	final Object D = iter.next();
+            final Object D0 = cov.op(0, D);
         	final INode v = (INode) ch2v.get(D);
-        	final INode w = (INode) ch2v.get(cov.op(0, D));
-        	final FreeWord word = (FreeWord) e2w.get(new Pair(new Integer(0), D));
-        	Vector s = Vector.zero(dim);
-        	for (int i = 0; i < word.length(); ++i) {
-        		final int k = word.getLetter(i) - 1;
-        		final int sign = word.getSign(i);
-        		if (sign > 0) {
-        			s = (Vector) s.plus(L[k]);
-        		} else {
-        			s = (Vector) s.minus(L[k]);
-        		}
-        	}
+        	final INode w = (INode) ch2v.get(D0);
+        	final Vector s = (Vector) edgeTranslation(0, D).plus(trans.get(D0)).minus(
+                    trans.get(D));
         	if (getEdge(v, w, s) == null) {
         		newEdge(v, w, s);
         	}
         }
 	}
+    
+    /**
+     * Helper method for computing the translation associated to an edge in the covering
+     * Delaney symbol.
+     * 
+     * @param idx the index of the edge.
+     * @param D the source element of the edge.
+     * @return the translation vector associated to the edge.
+     */
+    private Vector edgeTranslation(final int idx, final Object D) {
+        final FreeWord word = (FreeWord) edgeToWord.get(new Pair(new Integer(idx), D));
+        Vector s = Vector.zero(getDimension());
+        for (int i = 0; i < word.length(); ++i) {
+            final int k = word.getLetter(i) - 1;
+            final int sign = word.getSign(i);
+            if (sign > 0) {
+                s = (Vector) s.plus(L[k]);
+            } else {
+                s = (Vector) s.minus(L[k]);
+            }
+        }
+        return s;
+    }
 }
