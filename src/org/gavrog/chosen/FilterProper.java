23b6de69921d9ed2f2ee056dbf1021ef1ed4887e
odf <devnull@localhost>
2006-04-25 00:40:37 +0000

    New file.

---
+++ src/org/gavrog/chosen/FilterProper.java
@@ -0,0 +1,62 @@
+/*
+   Copyright 2005 Olaf Delgado-Friedrichs
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package org.gavrog.chosen;
+
+import org.gavrog.joss.crossover.Skeleton;
+import org.gavrog.joss.dsyms.basic.DSymbol;
+import org.gavrog.joss.dsyms.derived.Covers;
+import org.gavrog.joss.dsyms.generators.InputIterator;
+import org.gavrog.joss.pgraphs.basic.PeriodicGraph;
+
+
+/**
+ * A tiling is proper if it has the same symmetry as its underlying net.
+ * 
+ * @author Olaf Delgado
+ * @version $Id: FilterProper.java,v 1.1 2006/04/25 00:40:37 odf Exp $
+ */
+public class FilterProper {
+
+    public static void main(String[] args) {
+        final String filename = args[0];
+
+        int inCount = 0;
+        int outCount = 0;
+
+        for (final InputIterator input = new InputIterator(filename); input.hasNext();) {
+            final DSymbol ds = (DSymbol) input.next();
+            ++inCount;
+            if (isProper(ds)) {
+                ++outCount;
+                System.out.println(ds);
+            }
+        }
+
+        System.err.println("Read " + inCount + " symbols, " + outCount
+                           + " of which were proper and had stable graphs.");
+    }
+
+    private static boolean isProper(final DSymbol ds) {
+        final DSymbol min = new DSymbol(ds.minimal());
+        final DSymbol cov = new DSymbol(Covers.pseudoToroidalCover3D(min));
+        final PeriodicGraph gr = new Skeleton(cov);
+        if (!gr.isLocallyStable()) {
+            return false;
+        }
+        return gr.isMinimal() && gr.symmetries().size() == cov.size() / min.size();
+    }
+}
