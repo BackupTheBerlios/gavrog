fadb8b7d0184a9a5466b81857dd4cb23d413b863
odf <devnull@localhost>
2006-11-01 20:47:14 +0000

    New file.

---
+++ src/org/gavrog/joss/dsyms/generators/MakeTilesForFaceTransitive.java
@@ -0,0 +1,83 @@
+/*
+   Copyright 2006 Olaf Delgado-Friedrichs
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package org.gavrog.joss.dsyms.generators;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.gavrog.box.collections.IteratorAdapter;
+import org.gavrog.box.collections.Iterators;
+import org.gavrog.jane.fpgroups.FpGroup;
+import org.gavrog.jane.fpgroups.GroupAction;
+import org.gavrog.jane.fpgroups.SmallActionsIterator;
+import org.gavrog.jane.numbers.Fraction;
+import org.gavrog.joss.dsyms.basic.DSymbol;
+import org.gavrog.joss.dsyms.basic.IndexList;
+import org.gavrog.joss.dsyms.derived.Covers;
+import org.gavrog.joss.dsyms.derived.FundamentalGroup;
+
+
+/**
+ * Generates all spherical, non-degenerate 2d Delaney symbols with up to two
+ * faces of the same size.
+ * 
+ * @author Olaf Delgado
+ * @version $Id: MakeTilesForFaceTransitive.java,v 1.1 2006/11/01 20:47:14 odf Exp $
+ */
+public class MakeTilesForFaceTransitive extends IteratorAdapter {
+	final private Iterator actions;
+	private Iterator current;
+	final private FundamentalGroup G;
+
+    public MakeTilesForFaceTransitive(final int size) {
+        this.G = new FundamentalGroup(new DSymbol("1:1,1,1:0,0"));
+        final FpGroup pG = G.getPresentation();
+        this.actions = new SmallActionsIterator(pG, size, false);
+        this.current = Iterators.empty();
+    }
+    
+    protected Object findNext() throws NoSuchElementException {
+		while (true) {
+			if (current.hasNext()) {
+				return (DSymbol) current.next();
+			} else if (actions.hasNext()) {
+				final GroupAction action = (GroupAction) actions.next();
+				final DSymbol set = new DSymbol(Covers.cover(G, action));
+                if (set.numberOfOrbits(new IndexList(0, 1)) > 2) {
+                    //TODO make sure face degrees are equal and between 3 and 5
+                    continue;
+                }
+				current = new DefineBranching2d(set, 3, 3, new Fraction(1, 30));
+			} else {
+				throw new NoSuchElementException("at end");
+			}
+		}
+	}
+    
+    public static void main(final String[] args) {
+        int i = 0;
+        final int maxSize = args.length > i ? Integer.parseInt(args[i]) : 6;
+        final Iterator symbols = new MakeTilesForFaceTransitive(maxSize);
+
+        final long start = System.currentTimeMillis();
+        final int count = Iterators.print(System.out, symbols, "\n");
+        final long stop = System.currentTimeMillis();
+        System.out.println("\nGenerated " + count + " symbols.");
+        System.out.println("Execution time was " + (stop - start) / 1000.0
+                + " seconds.");
+    }
+}
