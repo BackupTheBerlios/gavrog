caa7995022935b110f5fce3b5f73a73401713fcc
odf <devnull@localhost>
2007-04-04 21:04:36 +0000

    New file.

---
+++ jython/identify.py
@@ -0,0 +1,47 @@
+#!/bin/env jython
+
+# ============================================================
+#   Global imports
+# ============================================================
+
+import sys
+
+from org.gavrog.joss.pgraphs.io import Net
+from org.gavrog.systre import Archive
+
+
+# ============================================================
+#   Prepare for RCSR and Zeolite Atlas lookup
+# ============================================================
+
+def archive_read(archive, path):
+    from java.lang import ClassLoader
+    from java.io import InputStreamReader, BufferedReader
+
+    # --- make sure this works from within .jar files and such
+    stream = ClassLoader.getSystemResourceAsStream(path)
+    reader = BufferedReader(InputStreamReader(stream))
+    archive.addAll(reader)
+
+# --- create an empty archive
+archive = Archive("1.0")
+
+# --- add entries from RCSR and zeolite archive files
+archive_read(archive, "org/gavrog/systre/rcsr.arc")
+archive_read(archive, "org/gavrog/systre/zeolites.arc")
+
+
+# ============================================================
+#   Main loop: read nets and print their symbols if found
+# ============================================================
+
+for G in Net.iterator(sys.argv[1]):
+    found = archive.get(G.minimalImage().systreKey)
+    if found:
+        print "%s" % found.name
+    else:
+        print ">>>unknown<<<"
+
+# ============================================================
+#   EOF
+# ============================================================
