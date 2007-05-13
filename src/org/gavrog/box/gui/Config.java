caf82cb84c99b4861e2519a2fc12a8b41840e23c
odf <devnull@localhost>
2007-05-13 19:37:40 +0000

    Added some methods.

--- src/org/gavrog/box/gui/Config.java
+++ src/org/gavrog/box/gui/Config.java
@@ -18,12 +18,17 @@
 package org.gavrog.box.gui;
 
 import java.awt.Color;
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author Olaf Delgado
- * @version $Id: Config.java,v 1.1 2007/05/13 07:55:29 odf Exp $
+ * @version $Id: Config.java,v 1.2 2007/05/13 19:37:40 odf Exp $
  */
 public class Config {
 	final private static Map mappedTypes = new HashMap();
@@ -36,11 +41,67 @@ public class Config {
 		mappedTypes.put(boolean.class, Boolean.class);
 	}
 	
-	public static Class wrapperType(final Class cl) {
-		if (mappedTypes.containsKey(cl)) {
-			return (Class) mappedTypes.get(cl);
+	public static Class wrapperType(final Class type) {
+		if (mappedTypes.containsKey(type)) {
+			return (Class) mappedTypes.get(type);
 		} else {
-			return cl;
+			return type;
 		}
+	}
+	
+	public static Object construct(final Class type, final String value)
+			throws Exception {
+		return wrapperType(type).getConstructor(new Class[] { String.class })
+				.newInstance(new Object[] { value });
+	}
+
+	public static String asString(final Object value) throws Exception {
+		final Class type = value.getClass();
+		final Class wrapperType = wrapperType(type);
+		if (wrapperType.equals(type)) {
+			return String.valueOf(value);
+		} else {
+			return String.valueOf(wrapperType(type).getConstructor(
+					new Class[] { type }).newInstance(new Object[] { value }));
+		}
+	}
+	
+	public static void setProperties(final Object target, final Properties input)
+			throws Exception {
+		final Class type = target.getClass();
+		final String prefix = type.getCanonicalName() + ".";
+		final BeanInfo info = Introspector.getBeanInfo(type);
+		final PropertyDescriptor props[] = info.getPropertyDescriptors();
+		for (int i = 0; i < props.length; ++i) {
+			final String name = props[i].getName();
+			if (name.equals("class")) {
+				continue;
+			}
+			final String value = input.getProperty(prefix + name);
+			if (value == null) {
+				continue;
+			}
+			final Method setter = props[i].getWriteMethod();
+			setter.invoke(target, new Object[] { construct(setter
+					.getParameterTypes()[0], value) });
+		}
+	}
+
+	public static Properties getProperties(final Object source)
+			throws Exception {
+		final Class type = source.getClass();
+		final String prefix = type.getCanonicalName() + ".";
+		final BeanInfo info = Introspector.getBeanInfo(type);
+		final Properties result = new Properties();
+		final PropertyDescriptor props[] = info.getPropertyDescriptors();
+		for (int i = 0; i < props.length; ++i) {
+			final String name = props[i].getName();
+			if (name.equals("class")) {
+				continue;
+			}
+			result.setProperty(prefix + name, asString(props[i].getReadMethod()
+					.invoke(source, new Object[] {})));
+		}
+		return result;
 	}
 }
