9f89165d0cb6568e4f669f43cd39b5a1f8e5780d
odf <devnull@localhost>
2007-05-13 00:01:43 +0000

    New class.

---
+++ src/org/gavrog/box/gui/OptionInputBox.java
@@ -0,0 +1,112 @@
+/*
+   Copyright 2006 Olaf Delgado-Friedrichs
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/*
+Copyright 2005 Olaf Delgado-Friedrichs
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package org.gavrog.box.gui;
+
+import java.awt.Insets;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gavrog.box.simple.Strings;
+
+import buoy.event.EventProcessor;
+import buoy.event.ValueChangedEvent;
+import buoy.widget.BLabel;
+import buoy.widget.BTextField;
+import buoy.widget.LayoutInfo;
+import buoy.widget.RowContainer;
+
+public class OptionInputBox extends RowContainer {
+	final private static Map mappedTypes = new HashMap();
+	static {
+		mappedTypes.put(int.class, Integer.class);
+		mappedTypes.put(long.class, Long.class);
+		mappedTypes.put(float.class, Float.class);
+		mappedTypes.put(double.class, Double.class);
+	}
+	
+	private BTextField input;
+
+	public OptionInputBox(
+			final String label, final Object target, final String option)
+			throws Exception {
+		this(label, target, option, 10);
+	}
+	
+	public OptionInputBox(
+			final String label, final Object target, final String option,
+			final int size) throws Exception {
+
+		super();
+		this.setBackground(null);
+		this.setDefaultLayout(new LayoutInfo(LayoutInfo.WEST, LayoutInfo.NONE,
+				new Insets(5, 5, 5, 5), null));
+
+		this.input = new BTextField(size);
+		this.add(input);
+		this.add(new BLabel(label));
+
+		final Class klazz = (target instanceof Class ? (Class) target : target
+				.getClass());
+		final String optionCap = Strings.capitalized(option);
+		final Method getter = klazz.getMethod("get" + optionCap, null);
+		final Method setter = klazz.getMethod("set" + optionCap,
+				new Class[] { int.class });
+		final Class optionType = mappedType(getter.getReturnType());
+		final Constructor constructor = optionType
+				.getConstructor(new Class[] { String.class });
+
+		this.input.setText(String.valueOf(getter.invoke(target, null)));
+
+		this.input.addEventLink(ValueChangedEvent.class, new EventProcessor() {
+			public void handleEvent(final Object event) {
+				try {
+					final Object value = constructor
+							.newInstance(new Object[] { input.getText() });
+					setter.invoke(target, new Object[] { value });
+				} catch (final Exception ex) {
+				}
+			}
+		});
+	}
+	
+	private Class mappedType(final Class type) {
+		if (mappedTypes.containsKey(type)) {
+			return (Class) mappedTypes.get(type);
+		} else {
+			return type;
+		}
+	}
+}
