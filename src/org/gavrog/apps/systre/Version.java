d42b2c98486f16298be114b2ea394675144ad4aa
odf <devnull@localhost>
2008-07-04 04:48:21 +0000

    Updated the RCSR archive. Documented Systre's built-in archives in the help file.

--- src/org/gavrog/apps/systre/Version.java
+++ src/org/gavrog/apps/systre/Version.java
@@ -25,7 +25,7 @@ public class Version {
 
 	final public static String maturity = "beta";
 
-	final public static String date = "2008/03/11";
+	final public static String date = "2008/07/04";
 
 	final public static String extension = (maturity == null ? "" : " "
             + maturity)
