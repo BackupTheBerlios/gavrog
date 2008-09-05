be4a46332772612d2f8b900a5b9bead211dd1863
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2008-09-05 11:04:32 +1000

    Updated.

--- src/org/gavrog/apps/_3dt/Version.java
+++ src/org/gavrog/apps/_3dt/Version.java
@@ -25,7 +25,7 @@ public class Version {
 
 	final public static String maturity = "alpha";
 
-	final public static String date = "2008/08/27";
+	final public static String date = "2008/09/04";
 
 	final public static String extension = (maturity == null ? "" : " "
             + maturity)
