40d78d23a9dc1a0d8f78d4a5faa4bb9fddc30440
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2008-10-08 15:42:28 +1100

    Updated.

--- src/org/gavrog/apps/_3dt/Version.java
+++ src/org/gavrog/apps/_3dt/Version.java
@@ -23,9 +23,9 @@ public class Version {
 
 	final public static int patchLevel = 0;
 
-	final public static String maturity = "beta";
+	final public static String maturity = "preview2";
 
-	final public static String date = "2008/10/02";
+	final public static String date = "2008/10/08";
 
 	final public static String extension = (maturity == null ? "" : " "
             + maturity)
