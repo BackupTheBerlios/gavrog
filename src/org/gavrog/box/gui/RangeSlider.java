2f2a05e32d88f24756d7262d647bfb704ac4b44c
Olaf Delgado-Friedrichs <odf@gavrog.org>
2008-11-16 19:07:38 +1100

    Start of new range slider class.

---
+++ src/org/gavrog/box/gui/RangeSlider.java
@@ -0,0 +1,135 @@
+/*
+   Copyright 2008 Olaf Delgado-Friedrichs
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+package org.gavrog.box.gui;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import buoy.event.MouseDraggedEvent;
+import buoy.event.MousePressedEvent;
+import buoy.event.MouseReleasedEvent;
+import buoy.event.RepaintEvent;
+import buoy.event.ValueChangedEvent;
+import buoy.event.WidgetMouseEvent;
+
+public class RangeSlider extends SliderBase {
+	private double lo;
+	private double hi;
+	private Point clickPos;
+	private boolean movingHi;
+
+	public RangeSlider(final double lo, final double hi, final double min,
+			final double max) {
+		this.min = min;
+		this.max = Math.max(min, max);
+		
+	    addEventLink(MousePressedEvent.class, this, "mousePressed");
+	    addEventLink(MouseReleasedEvent.class, this, "mouseReleased");
+	    addEventLink(MouseDraggedEvent.class, this, "mouseDragged");
+	    addEventLink(RepaintEvent.class, this, "paint");
+	    
+	    setLowValue(lo);
+	    setHighValue(hi);
+	}
+	
+	public Dimension getPreferredSize() {
+		final int width = showValue ? 210 : 150;
+		final int height = showTicks ? 14 : 11;
+		return new Dimension(width, height);
+	}
+	
+	public void paint(final RepaintEvent ev) {
+		final Graphics2D g = ev.getGraphics();
+		if (g == null) return;
+		
+		g.setStroke(new BasicStroke(1));
+		
+		clearCanvas(g);
+		drawGuide(g);
+		drawTicks(g);
+		if (showValue) {
+			showValue(g);
+		}
+		fillGuide(g, lo, hi);
+		drawMarker(g, lo);
+		drawMarker(g, hi);
+	}
+
+	protected void showValue(final Graphics2D g) {
+		final Font f = new Font("Verdana", Font.PLAIN, 10);
+		g.setFont(f);
+		g.setColor(new Color(0.0f, 0.4f, 0.6f));
+		final String s;
+		if (lo == (int) lo && hi == (int) hi) {
+			s = String.format("%d..%d", (int) lo, (int) hi);
+		} else {
+			s = String.format("%.2f..%.2f", lo, hi);
+		}
+		g.drawString(s, sliderWidth() + 8, 10);
+	}
+
+	protected int sliderWidth() {
+		return getBounds().width - 7 - (showValue ? 30 : 0);
+	}
+	
+	@SuppressWarnings("unused")
+	protected void mousePressed(MousePressedEvent ev) {
+		clickPos = ev.getPoint();
+		final int x = valueToX(value);
+		if (clickPos.x < x || clickPos.x > x + 4) {
+			clickPos = new Point(x, clickPos.y);
+			mouseDragged(ev);
+		}
+	}
+
+	protected void mouseDragged(WidgetMouseEvent ev) {
+		setValue(xToValue(ev.getPoint().x));
+	}
+
+	@SuppressWarnings("unused")
+	protected void mouseReleased(MouseReleasedEvent ev) {
+		final Point pos = ev.getPoint();
+		if (pos.x != clickPos.x) {
+			dispatchEvent(new ValueChangedEvent(this));
+		}
+	}
+	  
+	public double getLowValue() {
+		return lo;
+	}
+
+	public double getHighValue() {
+		return hi;
+	}
+
+	public void setLowValue(final double newValue) {
+		value = newValue;
+		if (value < min) value = min;
+		if (value > max) value = max;
+		if (snapInterval > 0) {
+			value = Math.round((value - min) / snapInterval) * snapInterval
+					+ min;
+		}
+		dispatchEvent(new RepaintEvent(this, (Graphics2D) getComponent()
+				.getGraphics()));
+	}
+}
