e5f5031620dc8b524971ddb3ae0478bc150e8ce8
Olaf Delgado-Friedrichs <odf@gavrog.org>
2008-11-24 14:25:47 +1100

    Made TileKTransitiveDuo resumable.

--- src/org/gavrog/joss/dsyms/generators/TileKTransitiveDuo.java
+++ src/org/gavrog/joss/dsyms/generators/TileKTransitiveDuo.java
@@ -16,20 +16,25 @@
 
 package org.gavrog.joss.dsyms.generators;
 
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import org.gavrog.box.collections.IteratorAdapter;
 import org.gavrog.box.collections.Iterators;
 import org.gavrog.box.collections.Pair;
+import org.gavrog.box.simple.Stopwatch;
 import org.gavrog.joss.dsyms.basic.DSymbol;
 import org.gavrog.joss.dsyms.basic.DelaneySymbol;
 import org.gavrog.joss.dsyms.basic.DynamicDSymbol;
 import org.gavrog.joss.dsyms.basic.IndexList;
 import org.gavrog.joss.dsyms.derived.Covers;
 import org.gavrog.joss.dsyms.derived.EuclidicityTester;
+
+import buoy.event.EventProcessor;
 
 /**
  * Generates all minimal, locally euclidean, tile-k-transitive tilings by a
@@ -38,7 +43,7 @@ import org.gavrog.joss.dsyms.derived.EuclidicityTester;
  * @author Olaf Delgado
  * @version $Id: TileKTransitiveDuo.java,v 1.1 2008/06/16 08:02:50 odf Exp $
  */
-public class TileKTransitiveDuo extends IteratorAdapter {
+public class TileKTransitiveDuo extends ResumableGenerator<DSymbol> {
     private final boolean verbose;
     private final boolean simple;
 
@@ -50,6 +55,9 @@ public class TileKTransitiveDuo extends IteratorAdapter {
     private int count3dSets = 0;
     private int count3dSymbols = 0;
     private int countMinimal = 0;
+    private int checkpoint[] = new int[] { 0, 0, 0 };
+    private int resume[] = new int[] { 0, 0, 0 };
+    private String resume1 = null;
 
     /**
      * Constructs an instance.
@@ -74,12 +82,45 @@ public class TileKTransitiveDuo extends IteratorAdapter {
         this.symbols = null;
     }
 
+    /**
+     * Sets the point in the search tree at which the algorithm should resume.
+     * 
+     * @param resume specifies the checkpoint to resume execution at.
+     */
+    public void setResumePoint(final String spec) {
+    	if (spec == null || spec.length() == 0) {
+    		return;
+    	}
+    	final String fields[] = spec.trim().split("-");
+    	this.resume[0] = Integer.valueOf(fields[0]);
+    	if (fields[1].startsWith("[")) {
+    		this.resume1 = fields[1].substring(1, fields[1].length() - 1)
+					.replaceAll("\\.", "-");
+    	} else {
+        	this.resume[1] = Integer.valueOf(fields[2]);
+        	this.resume[2] = Integer.valueOf(fields[2]);
+    	}
+    }
+
+    private boolean tooEarly() {
+    	if (checkpoint[0] != resume[0]) {
+    		return checkpoint[0] < resume[0];
+    	} else if (resume1 == null) {
+    		if (checkpoint[1] != resume[1]) {
+    			return checkpoint[1] < resume[1];
+    		} else {
+    			return checkpoint[2] < resume[2];
+    		}
+    	}
+    	return false;
+    }
+    
     /*
      * (non-Javadoc)
      * 
      * @see javaDSym.util.IteratorAdapter#findNext()
      */
-    protected Object findNext() throws NoSuchElementException {
+    protected DSymbol findNext() throws NoSuchElementException {
         while (true) {
             while (symbols == null || !symbols.hasNext()) {
                 while (extended == null || !extended.hasNext()) {
@@ -96,17 +137,38 @@ public class TileKTransitiveDuo extends IteratorAdapter {
                                 .hasNext();) {
                             tmp.append((DSymbol) iter.next());
                         }
+                        ++checkpoint[0];
+                        checkpoint[1] = checkpoint[2] = 0;
+                        postCheckpoint();
+                        if (tooEarly()) {
+                        	continue;
+                        }
                         final DSymbol ds = new DSymbol(tmp);
                         ++this.count2dSymbols;
                         if (this.verbose) {
                             System.out.println(setAsString(ds));
                         }
                         extended = extendTo3d(ds);
+                        if (extended instanceof ResumableGenerator) {
+                        	final ResumableGenerator gen =
+                        		(ResumableGenerator) extended;
+							gen.addEventLink(CheckpointEvent.class, this,
+									"dispatchEvent");
+							if (checkpoint[0] == resume[0] && resume1 != null) {
+								gen.setResumePoint(resume1);
+							}
+						}
                     } else {
                         throw new NoSuchElementException("At end");
                     }
                 }
                 final DSymbol ds = (DSymbol) extended.next();
+                ++checkpoint[1];
+                checkpoint[2] = 0;
+                postCheckpoint();
+                if (tooEarly()) {
+                	continue;
+                }
                 ++this.count3dSets;
                 if (this.verbose) {
                     System.out.println("    " + setAsString(ds));
@@ -114,6 +176,11 @@ public class TileKTransitiveDuo extends IteratorAdapter {
                 symbols = defineBranching(ds);
             }
             final DSymbol ds = (DSymbol) symbols.next();
+            ++checkpoint[2];
+            postCheckpoint();
+            if (tooEarly()) {
+            	continue;
+            }
             ++count3dSymbols;
             if (this.verbose) {
                 System.out.println("        " + branchingAsString(ds));
@@ -126,6 +193,26 @@ public class TileKTransitiveDuo extends IteratorAdapter {
         }
     }
 
+	private void postCheckpoint() {
+		dispatchEvent(new CheckpointEvent(this, tooEarly(), null));
+	}
+
+    /**
+     * Retreives the current checkpoint value as a string.
+     * 
+     * @return the current checkpoint.
+     */
+    public String getCheckpoint() {
+    	if (extended != null && extended instanceof ResumableGenerator) {
+			return String.format("%d-[%s]", checkpoint[0],
+					((ResumableGenerator) extended).getCheckpoint().replaceAll(
+							"-", "."));
+		} else {
+			return String.format("%s-%s-%s", checkpoint[0], checkpoint[1],
+					checkpoint[2]);
+		}
+	}
+    
     /**
      * Override this to restrict the equivariant tile combinations used.
      * 
@@ -181,13 +268,13 @@ public class TileKTransitiveDuo extends IteratorAdapter {
     protected Iterator extendTo3d(final DSymbol ds) {
 		if (this.simple) {
 			return new CombineTiles(ds) {
-				protected List getExtraDeductions(final DelaneySymbol ds,
+				protected List<Move> getExtraDeductions(final DelaneySymbol ds,
 						final Move move) {
-					final List out = new ArrayList();
+					final List<Move> out = new ArrayList<Move>();
 					final Object D = move.element;
 					Object E = D;
 					int r = 0;
-					List cuts = new ArrayList();
+					List<Object> cuts = new ArrayList<Object>();
 					do {
 						E = ds.op(2, E);
 						if (ds.definesOp(3, E)) {
@@ -252,70 +339,168 @@ public class TileKTransitiveDuo extends IteratorAdapter {
         return tmp.substring(i + 1);
     }
 
+	public static void usage() {
+		System.err.print(
+			  "Usage: java [PATH] [OPTION]... ds1 k1 ds2 k2 [FILE]\n"
+			+ "\n"
+			+ "where"
+			+ "  PATH = org.gavrog.joss.dsyms.generators.TileKTransitiveDuo\n"
+			+ "  ds1, ds2: D-symbols for topological tile types 1 and 2"
+			+ "  k1, k2:   numbers of tile orbits of types 1 and 2"
+			+ "\n"
+			+ "Recognized options:\n"
+			+ "  -e        skip euclidicity test\n"
+			+ "  -i N      interval in seconds between writing checkpoints\n"
+			+ "  -r A-B-C  resume generation at a checkpoint\n"
+			+ "  -s        generate only simple tilings\n"
+			+ "  -v        run in verbose mode\n"
+			);
+		System.exit(1);
+	}
+	
     public static void main(final String[] args) {
-        boolean verbose = false;
-        boolean check = true;
-        boolean simple = false;
-        int i = 0;
-        while (i < args.length && args[i].startsWith("-")) {
-            if (args[i].equals("-v")) {
-                verbose = !verbose;
-            } else if (args[i].equals("-e")) {
-                check = !check;
-            } else if (args[i].equals("-s")) {
-                simple = !simple;
-            } else {
-                System.out.println("Unknown option '" + args[i] + "'");
-                System.exit(1);
-            }
-            ++i;
-        }
+		try {
+			boolean verbose = false;
+			boolean check = true;
+			boolean simple = false;
+			int checkpointInterval = 3600;
+			String resume = null;
 
-        final DSymbol dsA = new DSymbol(args[i]);
-        final int nrA = Integer.parseInt(args[i + 1]);
-        final DSymbol dsB = new DSymbol(args[i + 2]);
-        final int nrB = Integer.parseInt(args[i + 3]);
-        final TileKTransitiveDuo iter =
-        	new TileKTransitiveDuo(dsA, nrA, dsB, nrB, simple, verbose);
-        int countGood = 0;
-        int countAmbiguous = 0;
+			int i = 0;
+			while (i < args.length && args[i].startsWith("-")) {
+				if (args[i].equals("-v")) {
+					verbose = !verbose;
+				} else if (args[i].equals("-e")) {
+					check = !check;
+				} else if (args[i].equals("-s")) {
+					simple = !simple;
+				} else if (args[i].equals("-i")) {
+					checkpointInterval = Integer.parseInt(args[++i]);
+				} else if (args[i].equals("-r")) {
+					resume = args[++i];
+				} else {
+					usage();
+				}
+				++i;
+			}
 
-        try {
-            while (iter.hasNext()) {
-                final DSymbol out = (DSymbol) iter.next();
-                if (check) {
-                    final EuclidicityTester tester = new EuclidicityTester(out);
-                    if (tester.isAmbiguous()) {
-                        System.out.println("# ??? " + out);
-                        ++countAmbiguous;
-                    } else if (tester.isGood()) {
-                        System.out.println(out);
-                        ++countGood;
-                    }
-                } else {
-                    System.out.println(out);
-                }
-                System.out.flush();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace(System.out);
-        }
+			if (args.length < i + 4) {
+				usage();
+			}
 
-        System.out.println("# " + iter.statistics());
-        if (check) {
-            System.out.println("# Of the latter, " + countGood
-                    + " were found euclidean.");
-            if (countAmbiguous > 0) {
-                System.out.println("# For " + countAmbiguous
-                        + " symbols, euclidicity could not yet be decided.");
-            }
-        }
-        System.out.println("# Options:");
-		System.out.println("#     simple:            "
-				+ (simple ? "on" : "off"));
-		System.out
-				.println("#     euclidicity check: " + (check ? "on" : "off"));
-		System.out.println("#     verbose:           "
-				+ (verbose ? "on" : "off"));
-    }
+			final DSymbol dsA = new DSymbol(args[i]);
+			final int nrA = Integer.parseInt(args[i + 1]);
+			final DSymbol dsB = new DSymbol(args[i + 2]);
+			final int nrB = Integer.parseInt(args[i + 3]);
+			i += 4;
+
+			final Writer output;
+			if (args.length > i + 1) {
+				output = new FileWriter(args[i + 1]);
+			} else {
+				output = new OutputStreamWriter(System.out);
+			}
+
+			int countGood = 0;
+			int countAmbiguous = 0;
+
+			final Stopwatch timer = new Stopwatch();
+			final Stopwatch eTestTimer = new Stopwatch();
+			timer.start();
+
+			output.write("# Program TileKTransitiveDuo.\n");
+			output.write("# Arguments:\n");
+			output.write("#     ds1 = " + dsA + "\n");
+			output.write("#     k1  = " + nrA + "\n");
+			output.write("#     ds2 = " + dsB + "\n");
+			output.write("#     k2  = " + nrB + "\n");
+			output.write("# Options:\n");
+			output.write("#     simple tilings only:             ");
+			output.write((simple ? "on" : "off") + "\n");
+			output.write("#     euclidicity test:                ");
+			output.write((check ? "on" : "off") + "\n");
+			output.write("#     verbose mode:                    ");
+			output.write((verbose ? "on" : "off") + "\n");
+			output.write("#     checkpoint interval:             ");
+			output.write(checkpointInterval + "sec\n");
+			if (resume != null) {
+				output.write(String.format("# Resuming at %s.\n", resume));
+			}
+			output.write("\n");
+			output.flush();
+			
+			final TileKTransitiveDuo iter = new TileKTransitiveDuo(dsA, nrA,
+					dsB, nrB, simple, verbose);
+			final Stopwatch chkptTimer = new Stopwatch();
+			final int interval = 1000 * checkpointInterval;
+	        iter.addEventLink(CheckpointEvent.class, new EventProcessor() {
+				@Override
+				public void handleEvent(final Object event) {
+					final CheckpointEvent ce = (CheckpointEvent) event;
+					if (ce.getMessage() != null || interval == 0
+							|| chkptTimer.elapsed() > interval) {
+						chkptTimer.reset();
+						final String msg = ce.getMessage();
+						final String p = ce.isOld() ? "# OLD" : "#@";
+						final String c = iter.getCheckpoint();
+						final String s =
+							msg != null ? String.format(" (%s)", msg) : "";
+						try {
+							output.write(String.format("%s CHECKPOINT %s%s\n",
+									p, c, s));
+							output.flush();
+						} catch (Throwable ex) {
+						}
+					}
+				}
+			});
+	        chkptTimer.start();
+			if (resume != null) {
+				iter.setResumePoint(resume);
+			}
+
+			for (final DSymbol out : iter) {
+				if (check) {
+					eTestTimer.start();
+					EuclidicityTester tester = new EuclidicityTester(out);
+					final boolean bad = tester.isBad();
+					final boolean ambiguous = tester.isAmbiguous();
+					eTestTimer.stop();
+					if (!bad) {
+						if (ambiguous) {
+							output.write("#@ name euclidicity dubious\n");
+							++countAmbiguous;
+						}
+						output.write(out + "\n");
+						++countGood;
+					}
+				} else {
+					output.write(out + "\n");
+				}
+				output.flush();
+			}
+			timer.stop();
+
+			output.write("\n");
+			output.write("# Total execution time in user mode was "
+					+ timer.format() + ".\n");
+			if (check) {
+				output.write("# Time for euclidicity tests was "
+						+ eTestTimer.format() + ".\n");
+			}
+			output.write("\n");
+			output.write("# " + iter.statistics() + "\n");
+			if (check) {
+				output.write("# Of those, " + countGood
+						+ " were found euclidean.\n");
+				if (countAmbiguous > 0) {
+					output.write("# For " + countAmbiguous
+							+ " symbols, euclidicity remains undetermined.\n");
+				}
+			}
+			output.flush();
+		} catch (Exception ex) {
+			ex.printStackTrace(System.err);
+		}
+	}
 }
