150aa6aa6f4f387d0a2dcc27c3c24c39e9ed7728
Olaf Delgado-Friedrichs <odf@gavrog.org>
2009-03-03 18:18:15 +1100

    Cyclo: generated graphs are now read and printed out in raw form as .pgr

--- src/org/gavrog/scratch/Cyclo.scala
+++ src/org/gavrog/scratch/Cyclo.scala
@@ -1,6 +1,6 @@
 package org.gavrog.scratch
 
-import scala.io._
+import java.io._
 import java.lang.ProcessBuilder
 
 object Cyclo {
@@ -22,12 +22,39 @@ object Cyclo {
   def generate(seq: List[Int]) {
     val args = "mgraph_p" :: seq.map(_.toString) ::: List("o", "p")
     val proc = new ProcessBuilder(args.toArray: _*).start
-
-    for (line <- Source.fromInputStream(proc.getErrorStream).getLines
-         if line.startsWith("Generated")) {
-      print(line)
-      total_count += line.split("\\s+")(1).toInt
+    val src = new DataInputStream(proc.getInputStream)
+    
+    def getDummy = try {
+      Some(src.readByte)
+    } catch {
+      case ex: EOFException => None
     }
+
+    def read(n: Int): Int = {
+      getDummy match {
+        case None => n
+        case Some(dummy) => {
+	      def next: Int = {
+	        if (dummy == 0) src.readUnsignedShort else src.readByte
+	      }
+	      val size: Int = if (dummy == 0) next else dummy
+          println("PERIODIC_GRAPH")
+	      var i = 1
+	      while (i < size) {
+	        val j = next
+	        if (j == 0) i += 1
+            else println("  %3d %3d" format (i, j))
+	      }
+          println("END")
+          read(n + 1)
+        }
+      }
+    }
+    val count = read(0)
+    proc.waitFor
+    
+    println("# Generated %d graphs for degree sequence %s." format (count, seq))
+    total_count += count
   }
   
   def main(args : Array[String]) : Unit = {
@@ -38,8 +65,8 @@ object Cyclo {
            l <- 0 to k)
         generate(0 :: l :: s)
 
-      println("=" * 72)
-      println("Generated altogeter %d graphs." format total_count)
+      println("# " + "=" * 72)
+      println("# Generated altogeter %d graphs." format total_count)
     } catch {
       case ex: Throwable => println(ex)
     }
