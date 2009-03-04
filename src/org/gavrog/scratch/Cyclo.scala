abd1087cd10bd9a167c7088d35665ceeec62acad
Olaf Delgado-Friedrichs <odf@gavrog.org>
2009-03-04 12:54:25 +1100

    Cyclo: graphs are simplified and edge lists are printed.

--- src/org/gavrog/scratch/Cyclo.scala
+++ src/org/gavrog/scratch/Cyclo.scala
@@ -1,9 +1,13 @@
 package org.gavrog.scratch
 
-import java.io._
+import java.io.{DataInputStream, EOFException, InputStream}
 import java.lang.ProcessBuilder
 
+import scala.collection.mutable.HashMap
+
 object Cyclo {
+  type Graph = Map[Int, List[Int]]
+  
   var total_count = 0
 
   def degree_sequences(n: Int, m: Int) = {
@@ -19,42 +23,68 @@ object Cyclo {
     seq(n, 2 * m, 3)
   }
   
-  def generate(seq: List[Int]) {
-    val args = "mgraph_p" :: seq.map(_.toString) ::: List("o", "p")
-    val proc = new ProcessBuilder(args.toArray: _*).start
-    val src = new DataInputStream(proc.getInputStream)
+  def read_graphs(is: InputStream) : Stream[Graph] = {
+    val src = new DataInputStream(is)
     
-    def getDummy = try {
+    val first_byte = try {
       Some(src.readByte)
     } catch {
       case ex: EOFException => None
     }
-
-    def read(n: Int): Int = {
-      getDummy match {
-        case None => n
-        case Some(dummy) => {
-	      def next: Int = {
-	        if (dummy == 0) src.readUnsignedShort else src.readByte
-	      }
-	      val size: Int = if (dummy == 0) next else dummy
-          println("PERIODIC_GRAPH")
-	      var i = 1
-	      while (i < size) {
-	        val j = next
-	        if (j == 0) i += 1
-            else println("  %3d %3d" format (i, j))
-	      }
-          println("END")
-          read(n + 1)
+    
+    first_byte match {
+      case None => Stream.empty
+      case Some(x) => {
+        def next: Int = if (x == 0) src.readUnsignedShort else src.readByte
+        val size: Int = if (x == 0) next else x
+        val adj = new HashMap[Int, List[Int]]
+        var i = 1
+        while (i < size) {
+          val j = next
+          if (j == 0) i += 1
+          else {
+            adj(i) = j :: adj.getOrElse(i, Nil)
+            adj(j) = i :: adj.getOrElse(j, Nil)
+          }
         }
+        Stream.cons(Map(adj.toSeq: _*), read_graphs(is))
       }
     }
-    val count = read(0)
-    proc.waitFor
+  }
+  
+  def simplified(gr: Graph) = {
+    var old2new  : Map[Int, Int]    = Map().withDefaultValue(0)
+    var new_edges: List[(Int, Int)] = Nil
     
-    println("# Generated %d graphs for degree sequence %s." format (count, seq))
-    total_count += count
+    for ((i, neighbors) <- gr)
+      if (neighbors.size == 2)
+        new_edges = (neighbors(0), neighbors(1)) :: new_edges
+      else old2new += (i -> (old2new.size + 1))
+    
+    val output = new HashMap[Int, List[Int]]
+    
+    for ((i, neighbors) <- gr if old2new(i) != 0)
+      output(old2new(i)) = neighbors map old2new filter (0 !=)
+    
+    for ((i, j) <- new_edges) {
+      val a = old2new(i)
+      val b = old2new(j)
+      output(a) = b :: output(a)
+      if (a != b) output(b) = a :: output(b)
+    }
+    
+    Map(output.toSeq: _*)
+  }
+  
+  def edges(gr: Graph) =
+    for ((i, neighbors) <- gr; j <- neighbors if i <= j) yield (i, j)
+  
+  def generate(seq: List[Int]) : Seq[Graph] = {
+    val args   = "mgraph_p" :: seq.map(_.toString) ::: List("o", "p")
+    val proc   = new ProcessBuilder(args.toArray: _*).start
+    val result = read_graphs(proc.getInputStream)
+    proc.waitFor
+    result
   }
   
   def main(args : Array[String]) : Unit = {
@@ -63,10 +93,20 @@ object Cyclo {
       for (n <- 0 to 2 * (k - 1);
            s <- degree_sequences(n, n + k - 1);
            l <- 0 to k)
-        generate(0 :: l :: s)
+      {
+        var count = 0
+        val sequence = 0 :: l :: s
+        for (g  <- generate(sequence) map simplified) {
+          println(edges(g) map (_.toString) reduceLeft(_ + _))
+          count += 1
+        }
+        println("# Generated %d graphs for degree sequence %s."
+                format (count, sequence))
+        total_count += count
+      }
 
       println("# " + "=" * 72)
-      println("# Generated altogeter %d graphs." format total_count)
+      println("# Generated a total of %d graphs." format total_count)
     } catch {
       case ex: Throwable => println(ex)
     }
