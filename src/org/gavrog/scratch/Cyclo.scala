b18e5766a6aae407f67668cbb255b3763369631c
Olaf Delgado-Friedrichs <odf@gavrog.org>
2009-03-04 16:09:53 +1100

    Cyclo: added coded for finding bridges.

--- src/org/gavrog/scratch/Cyclo.scala
+++ src/org/gavrog/scratch/Cyclo.scala
@@ -6,10 +6,13 @@ import java.lang.ProcessBuilder
 import scala.collection.mutable.HashMap
 
 object Cyclo {
+  class Pluralizer(n: Int) {
+    def apply(s: String) = "%d %s%s" format (n, s, if (n == 1) "" else "s")
+  }
+  implicit def i2pl(n: Int) = new Pluralizer(n)
+
   type Graph = Map[Int, List[Int]]
   
-  var total_count = 0
-
   def degree_sequences(n: Int, m: Int) = {
     def seq(n: Int, m: Int, min_d: Int) : Seq[List[Int]] =
       if (n * min_d > m)
@@ -53,8 +56,8 @@ object Cyclo {
   }
   
   def simplified(gr: Graph) = {
-    var old2new  : Map[Int, Int]    = Map().withDefaultValue(0)
-    var new_edges: List[(Int, Int)] = Nil
+    var old2new   = Map[Int, Int]().withDefaultValue(0)
+    var new_edges = Nil: List[(Int, Int)]
     
     for ((i, neighbors) <- gr)
       if (neighbors.size == 2)
@@ -79,6 +82,38 @@ object Cyclo {
   def edges(gr: Graph) =
     for ((i, neighbors) <- gr; j <- neighbors if i <= j) yield (i, j)
   
+  def bridges(gr: Graph) = {
+    var time   = 0
+    var dfsnum = Map[Int, Int]().withDefaultValue(0)
+    var low    = Map[Int, Int]().withDefaultValue(0)
+    var is_art = Set[Int]()
+    var result = Nil: List[(Int, Int)]
+    
+    def art(v: Int, u: Int) {
+      time += 1
+      low += (v -> time)
+      dfsnum += (v -> time)
+      
+      for (w <- gr(v) if w != u)
+        if (dfsnum(w) == 0) {
+          art(w, v)
+          if (low(w) < low(v)) low += (v -> low(w))
+          if ((dfsnum(v) == 1 &&
+                 (dfsnum(w) != 2 || !gr(v).forall(dfsnum(_) > 0)))
+              || (dfsnum(v) != 1 && low(w) >= dfsnum(v))
+          ) {
+            is_art += v
+            if (is_art(w) && gr(v).filter(w ==).size == 1)
+              result = (v, w) :: result
+          }
+        } else
+          if (dfsnum(w) < low(v)) low += (v -> dfsnum(w))
+    }
+    
+    art(gr.keys.next, 0)
+    result
+  }
+  
   def generate(seq: List[Int]) : Seq[Graph] = {
     val args   = "mgraph_p" :: seq.map(_.toString) ::: List("o", "p")
     val proc   = new ProcessBuilder(args.toArray: _*).start
@@ -90,26 +125,33 @@ object Cyclo {
   def main(args : Array[String]) : Unit = {
     try {
       val k = args(0).toInt
+      var n_total      = 0
+      var n_bridgeless = 0
+
       for (n <- 0 to 2 * (k - 1);
            s <- degree_sequences(n, n + k - 1);
            l <- 0 to k)
       {
         val graphs = generate(0 :: l :: s)
         val n = graphs.size
-        println("# Found %d graph%s with degree sequence (%s) and %d loop%s%s"
-                format (n, if (n == 1) "" else "s",
-                        s map (_.toString) reduceLeft(_ + ", " + _),
-                        l, if (l == 1) "" else "s",
-                        if (n > 0) ":" else "."))
-        for (g  <- graphs map simplified)
-          println(edges(g) map (_.toString) reduceLeft(_ + _))
-        total_count += n
+        println("# Found %s with degree sequence (%s) and %s%s"
+                format (n("graph"), s map (_.toString) reduceLeft(_ + ", " + _),
+                        l("loop"), if (n > 0) ":" else "."))
+        for (gr <- graphs) {
+          val b = bridges(gr).size
+          if (b == 0) n_bridgeless += 1
+          println("%s # (%s)" format
+                    (edges(simplified(gr)).map(_.toString).reduceLeft(_ + _),
+                     b("bridge")))
+        }
+        n_total += n
       }
 
       println
       println("# " + "=" * 72)
       println("# A total of %d graphs with cyclomatic number %d were generated."
-              format (total_count, k))
+              format (n_total, k))
+      println("# %d of those were bridgeless." format n_bridgeless)
     } catch {
       case ex: Throwable => println(ex)
     }
