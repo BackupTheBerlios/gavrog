45f52aeed3eff3ed408524802f4e8707b48d9fe3
Olaf Delgado-Friedrichs <odf@gavrog.org>
2009-03-06 15:35:30 +1100

    ToyStory: several changes.

--- src/org/gavrog/scratch/ToyStory.scala
+++ src/org/gavrog/scratch/ToyStory.scala
@@ -22,39 +22,39 @@ object ToyStory {
   }
 
   type Path = List[Move]
+  implicit def wrapped_path(p: Path) = new {
+    def cost = p.map(_.cost).reduceLeft(_ + _)
+    def text = p.dropRight(1).mkString("", ", ", "") + ", and " + p.last
+  }
 
   def pairs[A](s: List[A]) =
-    for ((first, index) <- s.toStream.zipWithIndex; second <- s.drop(index + 1))
+    for ((first, index) <- s.elements.zipWithIndex;
+         second <- s.elements.drop(index + 1))
       yield List(first, second)
   
-  def solutions(toys: List[Toy], limit: Int) = {
-    def solve_right(on_left: List[Toy],
-                    on_right: List[Toy], limit: Int) : Stream[Path] =
-      for (pair <- pairs(on_left);
-           move = Move(Right, pair);
-           remaining = limit - move.cost if remaining >= 0;
-           path <- solve_left(on_left -- pair, pair ::: on_right, remaining))
-        yield move :: path
+  def solve_right(on_left: List[Toy],
+                  on_right: List[Toy], limit: Int) : Iterator[Path] =
+    for { pair <- pairs(on_left)
+          move = Move(Right, pair)
+          remaining = limit - move.cost if remaining >= 0
+          path <- solve_left(on_left -- pair, pair ::: on_right, remaining) }
+      yield move :: path
 
-    def solve_left(on_left: List[Toy],
-                   on_right: List[Toy], limit: Int) : Stream[Path] =
-      on_left match {
-        case Nil => Stream(Nil)
-        case _ =>
-          for (toy  <- on_right.toStream;
-               move = Move(Left, List(toy));
-               remaining = limit - move.cost if remaining >= 0;
-               path <- solve_right(toy :: on_left, on_right - toy, remaining))
-            yield move :: path
-      }
+  def solve_left(on_left: List[Toy],
+                 on_right: List[Toy], limit: Int) : Iterator[Path] =
+    if (on_left == Nil) Iterator.single(Nil)
+    else for { toy <- on_right.elements
+               move = Move(Left, List(toy))
+               remaining = limit - move.cost if remaining >= 0
+               path <- solve_right(toy :: on_left, on_right - toy, remaining) }
+      yield move :: path
     
-    solve_right(toys, Nil, limit)
-  }
+  def solutions(toys: List[Toy], limit: Int) = solve_right(toys, Nil, limit)
 
   val toys =
     List(Toy("Buzz", 5), Toy("Woody", 10), Toy("Rex", 20), Toy("Hamm", 25))
 
   def main(args : Array[String]) : Unit =
     for (path <- solutions(toys, 60))
-      println("Solution: " + path.mkString("", ", ", "."))
+      println("In %d minutes, %s." format (path.cost, path.text))
 }
