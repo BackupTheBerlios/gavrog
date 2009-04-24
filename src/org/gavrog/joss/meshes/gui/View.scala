95c76107c62c12699e560d532e13fcebf09838aa
Olaf Delgado-Friedrichs <odf@gavrog.org>
2009-04-24 13:16:13 +1000

    Scala Mesh Viewer: added subdivide, coarsen and save actions.

--- src/org/gavrog/joss/meshes/gui/View.scala
+++ src/org/gavrog/joss/meshes/gui/View.scala
@@ -18,6 +18,7 @@
 package org.gavrog.joss.meshes.gui
 
 import java.awt.Color
+import java.io.{File, FileWriter}
 
 import scala.swing.{Action, BorderPanel, MainFrame, Menu, MenuBar,
                     MenuItem, Orientation, Separator, SplitPane, TextArea}
@@ -47,18 +48,32 @@ object View {
       }
       import meshLoader._
       
+      val meshSaver = new FileChoiceAction("Save mesh...", main) {
+        accelerator = "ctrl S"
+        openForWrite = true
+        override def openFile(selected: File) {
+          Mesh.write(new FileWriter(selected), Seq(mesh), null)
+    	  log("Wrote mesh to %s" format selected)
+    	}
+      }
+      
       val screenShotSaver =
         new ScreenShotAction("Take Screen Shot...", main, sceneViewer) {
           accelerator = "ctrl I"
         }
 
+      var _mesh: Mesh = null
+      def mesh = _mesh
+      def mesh_=(new_mesh: Mesh) {
+        _mesh = new_mesh
+        sceneViewer.setMesh(_mesh)
+        uvMapViewer.setMesh(_mesh)
+      }
+      
       listenTo(meshLoader, screenShotSaver)
       reactions += {
         case MessageSent(src, text) => log(text)
-        case MeshLoaded(src, mesh) => {
-          sceneViewer.setMesh(mesh)
-          uvMapViewer.setMesh(mesh)
-        }
+        case MeshLoaded(src, new_mesh) => mesh = new_mesh
         case ChoiceCancelled(src) => log("Cancelled!")
         case ChoiceError(src) => log("Error in file chooser.")
       }
@@ -73,10 +88,20 @@ object View {
         contents += new Menu("File") {
           contents ++ List(
             new MenuItem(meshLoader),
+            new MenuItem(meshSaver),
             new Separator,
             new MenuItem(screenShotSaver),
             new Separator,
-            new MenuItem(Action("Exit") { System.exit(0) })
+            new MenuItem(new Action("Exit") {
+              def apply() { System.exit(0) }
+              accelerator = "ctrl Q"
+            })
+          )
+        }
+        contents += new Menu("Mesh") {
+          contents ++ List(
+            new MenuItem(Action("Subdivide") { mesh = mesh.subdivision }),
+            new MenuItem(Action("Coarsen") { mesh = mesh.coarsening })
           )
         }
       }
